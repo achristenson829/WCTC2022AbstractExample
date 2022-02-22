12dd44f969c80c6c84460088d9f4a2c5c6ebfd5d
achristenson829 <achristenson829@gmail.com>
2022-02-21 20:50:52 -0600

    Added Snake Class

---
+++ Models/Snake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AbstractExample.Models
+{
+    public class Snake: Animal
+    {
+        public Snake()
+        {
+            Name = "Salazar";
+        }
+        public Snake(string name)
+        {
+            Name = name;
+        }
+
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} says Hissssssss");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine($"{Name} sleeps with both eyes open");
+        }
+    }
+}
